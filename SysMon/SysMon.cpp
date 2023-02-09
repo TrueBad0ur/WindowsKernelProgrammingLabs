65471083101fe26bd165cbaad0e34ad34e201432
truebad0ur <X@X.X>
2023-02-09 23:10:46 +0300

    SysMon with ImageFileName

---
+++ SysMon/SysMon.cpp
@@ -0,0 +1,213 @@
+#include "pch.h"
+#include "SysMon.h"
+#include "SysMonCommon.h"
+#include "AutoLock.h"
+
+#pragma warning(disable:4996 4616)
+
+DRIVER_UNLOAD SysMonUnload;
+DRIVER_DISPATCH SysMonCreateClose, SysMonRead;
+void OnProcessNotify(_Inout_ PEPROCESS Process, _In_ HANDLE ProcessId, _Inout_opt_ PPS_CREATE_NOTIFY_INFO CreateInfo);
+void OnThreadNotify(_In_ HANDLE ProcessId, _In_ HANDLE ThreadId, _In_ BOOLEAN Create);
+void OnImageLoadNotify(_In_opt_ PUNICODE_STRING FullImageName, _In_ HANDLE ProcessId, _In_ PIMAGE_INFO ImageInfo);
+void PushItem(LIST_ENTRY* entry);
+NTSTATUS OnRegistryNotify(PVOID context, PVOID arg1, PVOID arg2);
+
+Globals g_Globals;
+
+extern "C" NTSTATUS
+DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
+	UNREFERENCED_PARAMETER(RegistryPath);
+	auto status = STATUS_SUCCESS;
+	InitializeListHead(&g_Globals.ItemsHead);
+	g_Globals.Mutex.Init();
+
+	PDEVICE_OBJECT DeviceObject = nullptr;
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\SysMon");
+	bool symLinkCreated = false;
+
+	do {
+		UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\SysMon");
+		status = IoCreateDevice(DriverObject, 0, &devName, FILE_DEVICE_UNKNOWN, 0, TRUE, &DeviceObject);
+
+		if (!NT_SUCCESS(status)) {
+			KdPrint((DRIVER_PREFIX "failed to create device (0x%08X)\n", status));
+			break;
+		}
+
+		DeviceObject->Flags |= DO_DIRECT_IO;
+		status = IoCreateSymbolicLink(&symLink, &devName);
+
+		if (!NT_SUCCESS(status)) {
+			KdPrint((DRIVER_PREFIX "failed to create sym link (0x%08X)\n", status));
+			break;
+		}
+
+		symLinkCreated = true;
+
+		status = PsSetCreateProcessNotifyRoutineEx(OnProcessNotify, FALSE);
+		if (!NT_SUCCESS(status)) {
+			KdPrint((DRIVER_PREFIX "failed to register process callback (0x%08X)\n", status));
+			break;
+		}
+	} while (false);
+
+	if (!NT_SUCCESS(status)) {
+		if (symLinkCreated)
+			IoDeleteSymbolicLink(&symLink);
+		if (DeviceObject)
+			IoDeleteDevice(DeviceObject);
+	}
+
+	DriverObject->DriverUnload = SysMonUnload;
+	DriverObject->MajorFunction[IRP_MJ_CREATE] = SysMonCreateClose;
+	DriverObject->MajorFunction[IRP_MJ_CLOSE] = SysMonCreateClose;
+	DriverObject->MajorFunction[IRP_MJ_READ] = SysMonRead;
+	return status;
+}
+
+void OnProcessNotify(PEPROCESS Process, HANDLE ProcessId, PPS_CREATE_NOTIFY_INFO CreateInfo) {
+	UNREFERENCED_PARAMETER(Process);
+
+	if (CreateInfo) {
+		// process created
+		USHORT allocSize = sizeof(FullItem<ProcessCreateInfo>);
+		USHORT commandLineSize = 0;
+
+		if (CreateInfo->CommandLine) {
+			commandLineSize = CreateInfo->CommandLine->Length;
+			allocSize += commandLineSize;
+		}
+
+		USHORT ImageFileNameSize = 0;
+		if (CreateInfo->ImageFileName) {
+			ImageFileNameSize = CreateInfo->ImageFileName->Length + 2;
+			allocSize += ImageFileNameSize;
+		}
+
+		auto info = (FullItem<ProcessCreateInfo>*)ExAllocatePoolWithTag(PagedPool, allocSize, DRIVER_TAG);
+		if (info == nullptr) {
+			KdPrint((DRIVER_PREFIX "failed allocation\n"));
+			return;
+		}
+
+		auto& item = info->Data;
+		KeQuerySystemTimePrecise(&item.Time);
+		item.Type = ItemType::ProcessCreate;
+		item.Size = sizeof(ProcessCreateInfo) + commandLineSize + ImageFileNameSize;
+		item.ProcessId = HandleToULong(ProcessId);
+		item.ParentProcessId = HandleToULong(CreateInfo->ParentProcessId);
+
+		if (commandLineSize > 0) {
+			::memcpy((UCHAR*)&item + sizeof(item), CreateInfo->CommandLine->Buffer, commandLineSize);
+			item.CommandLineLength = commandLineSize / sizeof(WCHAR);	// length in WCHARs
+			item.CommandLineOffset = sizeof(item);
+		} else {
+			item.CommandLineLength = 0;
+			item.CommandLineOffset = 0;
+		}
+
+		if (ImageFileNameSize > 0) {
+			::memcpy((UCHAR*)&item + sizeof(item) + commandLineSize, CreateInfo->ImageFileName->Buffer, ImageFileNameSize);
+			item.ImageFileNameLength = ImageFileNameSize / sizeof(WCHAR);
+			item.ImageFileNameOffset = sizeof(item) + commandLineSize;
+		} else {
+			item.ImageFileNameLength = 0;
+			item.ImageFileNameOffset = 0;
+		}
+
+		PushItem(&info->Entry);
+
+	} else {
+		// process exited
+		auto info = (FullItem<ProcessExitInfo>*)ExAllocatePoolWithTag(PagedPool, sizeof(FullItem<ProcessExitInfo>), DRIVER_TAG);
+		if (info == nullptr) {
+			KdPrint((DRIVER_PREFIX "failed allocation\n"));
+			return;
+		}
+
+		auto& item = info->Data;
+		KeQuerySystemTimePrecise(&item.Time);
+
+		item.Type = ItemType::ProcessExit;
+		item.ProcessId = HandleToULong(ProcessId);
+		item.Size = sizeof(ProcessExitInfo);
+
+		PushItem(&info->Entry);
+	}
+}
+
+void PushItem(LIST_ENTRY* entry) {
+	AutoLock<FastMutex> lock(g_Globals.Mutex);
+	// TODO: get value of maximum from registry: ZwOpenKey, IoOpenDeviceRegistryKey, ZwQueryValueKey
+	if (g_Globals.ItemCount > 1024) {
+		// Слишком много элементов, удалить самый старый
+		auto head = RemoveHeadList(&g_Globals.ItemsHead);
+		g_Globals.ItemCount--;
+		auto item = CONTAINING_RECORD(head, FullItem<ItemHeader>, Entry);
+		ExFreePool(item);
+	}
+	InsertTailList(&g_Globals.ItemsHead, entry);
+	g_Globals.ItemCount++;
+}
+
+NTSTATUS SysMonCreateClose(PDEVICE_OBJECT, PIRP Irp) {
+	Irp->IoStatus.Status = STATUS_SUCCESS;
+	Irp->IoStatus.Information = 0;
+	IoCompleteRequest(Irp, 0);
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS SysMonRead(PDEVICE_OBJECT, PIRP Irp) {
+	auto stack = IoGetCurrentIrpStackLocation(Irp);
+	auto len = stack->Parameters.Read.Length;
+	auto status = STATUS_SUCCESS;
+	auto count = 0;
+	NT_ASSERT(Irp->MdlAddress); // Используем прямой ввод/вывод
+	auto buffer = (UCHAR*)MmGetSystemAddressForMdlSafe(Irp->MdlAddress, NormalPagePriority);
+
+	if (!buffer) {
+		status = STATUS_INSUFFICIENT_RESOURCES;
+	}
+	else {
+		AutoLock<FastMutex> lock(g_Globals.Mutex);
+		while (true) {
+			if (IsListEmpty(&g_Globals.ItemsHead)) // также можно проверить
+			// g_Globals.ItemCount
+				break;
+			auto entry = RemoveHeadList(&g_Globals.ItemsHead);
+			auto info = CONTAINING_RECORD(entry, FullItem<ItemHeader>, Entry);
+			auto size = info->Data.Size;
+			if (len < size) {
+				// Пользовательский буфер заполнен, вставить элемент обратно
+				InsertHeadList(&g_Globals.ItemsHead, entry);
+				break;
+			}
+			g_Globals.ItemCount--;
+			::memcpy(buffer, &info->Data, size);
+			len -= size;
+			buffer += size;
+			count += size;
+			// Освободить данные после копирования
+			ExFreePool(info);
+		}
+	}
+		Irp->IoStatus.Status = status;
+
+		Irp->IoStatus.Information = count;
+		IoCompleteRequest(Irp, 0);
+		return status;
+}
+
+void SysMonUnload(PDRIVER_OBJECT DriverObject) {
+	// Отмена регистрации уведомлений процессов
+	PsSetCreateProcessNotifyRoutineEx(OnProcessNotify, TRUE);
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\SysMon");
+	IoDeleteSymbolicLink(&symLink);
+	IoDeleteDevice(DriverObject->DeviceObject);
+	// Освобождение оставшихся элементов
+	while (!IsListEmpty(&g_Globals.ItemsHead)) {
+		auto entry = RemoveHeadList(&g_Globals.ItemsHead);
+		ExFreePool(CONTAINING_RECORD(entry, FullItem<ItemHeader>, Entry));
+	}
+}
